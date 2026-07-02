9c54ab22561ed2ff96820627ba354a81abf039be
Mykhailo Kremniov <mkremniov@gmail.com>
2026-07-02 20:31:58 +0300

    Remove the unused generated file "home_nano_nbgl.png", "mask_14x14.gif" used to generate it and the generation logic in build.rs

--- build.rs
+++ build.rs
@@ -18,44 +18,8 @@
 
 use std::process::Command;
 
-use image::{ImageFormat, ImageReader, Pixel};
-
-// TODO: all image files currently contain the Rust logo; need to replace them with Mintlayer logo.
-// See https://github.com/mintlayer/mintlayer-ledger-app/issues/10.
-
 fn main() {
     println!("cargo:rerun-if-changed=script.ld");
-    println!("cargo:rerun-if-changed=media/icons/mintlayer_14x14.gif");
-    println!("cargo:rerun-if-changed=media/icons/mask_14x14.gif");
-
-    let icons_path = std::path::PathBuf::from("media/icons");
-    let mut gray = ImageReader::open(icons_path.join("mintlayer_14x14.gif"))
-        .unwrap()
-        .decode()
-        .unwrap()
-        .into_luma8();
-
-    // Apply mask
-    let mask = ImageReader::open(icons_path.join("mask_14x14.gif"))
-        .unwrap()
-        .decode()
-        .unwrap()
-        .into_luma8();
-
-    for (x, y, mask_pixel) in mask.enumerate_pixels() {
-        let mask_value = mask_pixel[0];
-        let mut gray_pixel = *gray.get_pixel(x, y);
-        if mask_value == 0 {
-            gray_pixel = image::Luma([0]);
-        } else {
-            gray_pixel.invert();
-        }
-        gray.put_pixel(x, y, gray_pixel);
-    }
-
-    let glyph_path = std::path::PathBuf::from("media/glyphs");
-    gray.save_with_format(glyph_path.join("home_nano_nbgl.png"), ImageFormat::Png)
-        .unwrap();
 
     let output = Command::new("git")
         .args(["rev-parse", "HEAD"])
